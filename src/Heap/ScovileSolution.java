d1ccce0bdc086491ee23811f78ab359fd0794e98
hi-jung <wkdgmlwjd45@gmail.com>
2021-08-09 15:58:07 +0900

    다시 커밋

--- src/Heap/ScovileSolution.java
+++ src/Heap/ScovileSolution.java
@@ -1,2 +1,27 @@
-package Heap;public class ScovileSolution {
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class ScovileSolution {
+
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();
+
+        for (Integer s : scoville) {
+            priorityQueue.add(s);
+        }
+
+        while (priorityQueue.peek() < K) {
+            if (priorityQueue.size() == 1) return -1;
+
+
+            int newScovile = (priorityQueue.poll() + (priorityQueue.poll() * 2));
+            priorityQueue.add(newScovile);
+
+            answer++;
+        }
+        return  answer;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import Heap.ScovileSolution;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int K = 7;
+        int[] scovile = new int[] {1,2,3,9,10,12};
+
+        ScovileSolution scovileSoultion = new ScovileSolution();
+
+        System.out.println("================= Result ================");
+        System.out.println(scovileSoultion.solution(scovile, K));
+        System.out.println("=========================================");
+    }
 }
